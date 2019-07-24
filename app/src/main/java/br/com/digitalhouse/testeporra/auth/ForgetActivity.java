87606c71c1cebd5f3e1bee8a25160f6afd1600fc
percivalli <khaos36502838>
2019-07-24 19:20:55 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/testeporra/auth/ForgetActivity.java
@@ -0,0 +1,62 @@
+package br.com.digitalhouse.testeporra.auth;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.digitalhouse.testeporra.R;
+
+public class ForgetActivity extends AppCompatActivity {
+
+    EditText email;
+    Button btnNewPass;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_forget);
+        email = (EditText) findViewById(R.id.email_forgot);
+        btnNewPass = (Button) findViewById(R.id.new_password_forgot);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnNewPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email_value = email.getText().toString();
+
+                if (TextUtils.isEmpty(email_value)) {
+                    Toast.makeText(getApplicationContext(), "Please fill e-mail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.sendPasswordResetEmail(email_value)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Password reset link was sent your email address", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Mail sending error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+}
+
+
+
